13f7b3315b5dc1acd6a844ca38dff1e9c7a9a660
Manish-22 <manishreddym22@gmail.com>
2020-10-01 20:55:51 +0530

    initial commit

---
+++ queueimp.c
@@ -0,0 +1,130 @@
+#include "queue.h"
+static node *create_node(int id, int time, node *link)
+{
+	node* temp =  (node*)malloc(sizeof(node));
+	temp -> id = id; 
+	temp -> link = link;
+	temp -> time = time;
+	return temp;
+}
+void list_initialize(List *ptr_list)
+{
+    ptr_list->head=NULL;
+    ptr_list->number_of_nodes=0;
+	ptr_list->tail=NULL;	
+}
+const int node_get_id(node *node_ptr)
+{
+	return node_ptr->id;
+}
+
+const int node_get_time(node *node_ptr)
+{
+	return node_ptr->time;	
+}
+
+void list_insert_rear(List *ptr_list, int id, int time)
+{
+    node * t=create_node(id,time,NULL);
+	if(ptr_list->number_of_nodes==0)
+    {
+	    ptr_list->tail=t;
+		ptr_list->head=t;
+    }
+    else
+    {
+        ptr_list->tail->link=t;
+        ptr_list->tail=t;
+    }
+	ptr_list->number_of_nodes++;
+}
+
+void list_delete_front(List *ptr_list)
+{
+	if(ptr_list->number_of_nodes>0)
+    {
+        ptr_list->head=ptr_list->head->link;
+        ptr_list->number_of_nodes--;
+    }
+	else if(ptr_list->number_of_nodes==1)
+	{
+		ptr_list->tail=NULL;
+	}
+}
+
+void list_destroy(List *ptr_list)
+{
+    node *t, *u=NULL;
+	t=ptr_list->head;
+	while (t != NULL){
+		u=t;
+		t=t->link;
+		free(u);
+	}
+	free(ptr_list);
+}
+
+void queue_initialize(Queue *queue_list)
+{
+    List* ptr_list =(List*) malloc(sizeof(List));
+    queue_list->ptr_list=ptr_list;
+    list_initialize(queue_list->ptr_list);
+
+}
+
+void queue_enqueue(Queue *ptr, int id, int time)
+{
+    
+	list_insert_rear(ptr->ptr_list,id,time);
+
+}
+
+void queue_dequeue(Queue *ptr)
+{
+	list_delete_front(ptr->ptr_list);
+}
+
+int queue_is_empty(Queue *ptr)
+{
+	if(ptr->ptr_list->number_of_nodes==0)
+		return 1;
+	else
+		return 0;
+//TODO		
+}
+
+void queue_peek(Queue *ptr)
+{
+	if(queue_is_empty(ptr))
+		printf("Empty Queue\n");
+	else
+		printf("%d %d\n",ptr->ptr_list->head->id,ptr->ptr_list->head->time);
+}
+
+void queue_destroy(Queue *ptr)
+{
+    list_destroy(ptr->ptr_list);
+    free(ptr);
+}
+
+const int queue_find_person(Queue *ptr_queue, int t)
+{
+	int time=0;
+	while (time<t &&!queue_is_empty(ptr_queue))
+	{
+	    time+=ptr_queue->ptr_list->head->time;
+		if(time<t)
+			queue_dequeue(ptr_queue);
+	}
+	if (queue_is_empty(ptr_queue))
+	{
+		printf("Empty Queue\n");
+	}
+	else
+	{
+		printf("%d\n",ptr_queue->ptr_list->head->id);
+	}
+	
+	
+
+}
